cadb02df4643ada17cbc82f1a0ff9ae4f0945e17
fabszzz807 <fabriciofernandez807@gmail.com>
2025-06-24 19:35:14 -0300

    Update sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -25,29 +25,34 @@ while True:
             continue
         else:
             saldo += deposito
-            extrato += f"Deposito: R${deposito:.2f} | "
-            print(f"Saldo: {saldo:.0f}")
+            extrato += f"Deposito: R$ {deposito:.2f}\n"
+            print(f"Saldo: R$ {saldo:.2f}")
 
     elif opcao == "s":
         print("Saque")
         saque = float(input("Quanto voce quer sacar: "))
-        if numero_saques < 3:
+
+        if saque > 0:
             if saque <= saldo:
-                if saque <= limite:
-                    numero_saques += 1
-                    saldo -= saque
-                    print(f"Saldo restante: {saldo}")
-                    extrato += f"Saque: {saque:.2f} | "
-                    print(numero_saques)
+                if numero_saques < LIMITE_SAQUES:
+                    if saque <= limite:
+                        numero_saques += 1
+                        saldo -= saque
+                        print(f"Saldo restante: R$ {saldo:.2f}")
+                        extrato += f"Saque: R$ {saque:.2f}\n"
+                    else:
+                        print("Operacao falhou! O valor do saque excede o limite.")
                 else:
-                    print("Maior que o limite")
+                    print("Operacao falhou! Voce nao tem saldo suficiente.")
             else:
-                print("Saldo insuficiente")
+                print("Operacao falhou! Numero maximo de saques excedido.")
         else:
-            print("Limite de saques excedido")
+            print("Saque nao pode ser negativo")
     elif opcao == "e":
-        print("Extrato")
-        print(extrato)
+        print("\n============ Extrato ============")
+        print("Nao foram realizadas movimentacoes." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=================================")
     elif opcao == "q":
         break
     else:
