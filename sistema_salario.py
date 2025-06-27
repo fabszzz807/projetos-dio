199b479fe96f021ed4239b7ba33f7280bf9a99d6
fabszzz807 <fabriciofernandez807@gmail.com>
2025-06-26 21:27:08 -0300

    update Sistema Salario

--- sistema_salario.py
+++ sistema_salario.py
@@ -1,16 +1,16 @@
 salario_bruto = float(input("Salario bruto: "))
 adicional_beneficios = float(input("Beneficios: "))
-aliquota = 0
+valor_imposto = 0
 
 if salario_bruto >= 0 and salario_bruto <= 1100:
-    aliquota = salario_bruto * 0.05
+    valor_imposto = salario_bruto * 0.05
 elif salario_bruto > 1100 and salario_bruto <= 2500:
-    aliquota = salario_bruto * 0.1
+    valor_imposto = salario_bruto * 0.1
 elif salario_bruto > 2500:
-    aliquota = salario_bruto * 0.15
+    valor_imposto = salario_bruto * 0.15
 else:
     print("Valor invalido")
 
-salario_total = (salario_bruto - aliquota) + adicional_beneficios
+salario_total = (salario_bruto - valor_imposto) + adicional_beneficios
 
 print(f"Salario final: R$ {salario_total:.2f}")
